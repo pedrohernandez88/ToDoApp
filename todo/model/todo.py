326cc93c181d184a516f73db1a62eb0632ec75b5
Pedro Hernandez <phernandez804@soyudemedellin.edu.co>
2024-08-22 12:41:28 -0500

    add class todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,17 @@
-# TODO: Add code here
+class todo:
+  def __init__(self, code_id: int, title: str, description: str):
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = False
+        self.tags = []
+
+def mark_completed(self):
+    self.completed=True
+
+def add_tag(self, tag:str):
+    if tag not in self.tags:
+        self.tags.append(tag)
+
+def _str_(self):
+    return f"{self.code_id} - {self.title}"
